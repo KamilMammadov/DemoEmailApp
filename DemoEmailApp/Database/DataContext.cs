4915402c89620df84ae84c9aff3f46bb1a674d11
KamilMammadov <kamiltm@code.edu.az>
2022-11-26 16:29:28 +0400

    initual commit

---
+++ DemoEmailApp/Database/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoEmailApp.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+            : base(options)
+            
+        {
+
+        }
+
+    }
+}
---
+++ DemoEmailApp/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoEmailApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services
+                .AddDbContext<DataContext>(o =>
+                {
+                    o.UseSqlServer(builder.Configuration.GetConnectionString("MahmoodPC"));
+                })
+                .AddMvc()
+                .AddRazorRuntimeCompilation();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=home}/{action=index}");
+
+            app.Run();
+        }
+    }
+}
