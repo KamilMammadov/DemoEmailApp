4613af8ea506cd0e2f3ebf6cb32b9de790ea2df2
KamilMammadov <kamiltm@code.edu.az>
2022-11-26 23:54:51 +0400

    Controller and view created

--- DemoEmailApp/Controllers/EmailController.cs
+++ DemoEmailApp/Controllers/EmailController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DemoEmailApp.AppDataBase;
+using DemoEmailApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DemoEmailApp.Controllers
 {
     public class EmailController : Controller
     {
+        private readonly DataContext _dataContext;
+    
+        public EmailController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+           
+        }
+
+
         public IActionResult List()
         {
-            return View();
+            var model = _dataContext.Notifications
+                .Select(e=> new ListViewModel(e.Tittle,e.From,e.Message,e.TargetEmail))
+                .ToList();
+
+            return View(model);
+
+           
         }
     }
 }
---
+++ DemoEmailApp/ViewModels/ListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DemoEmailApp.Database;
+
+namespace DemoEmailApp.ViewModels
+{
+    public class ListViewModel
+    {
+       
+        public string From { get; set; }
+        public string Tittle { get; set; }
+        public string Message { get; set; }
+        public Email ToEmail { get; set; }
+
+      
+        public ListViewModel(string from, string tittle, string message, Email toemail)
+        {
+            From = from;
+            Tittle = tittle;
+            Message = message;
+             ToEmail = toemail;
+        }
+
+    }
+}
