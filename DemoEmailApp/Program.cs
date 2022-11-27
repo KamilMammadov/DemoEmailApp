70d5c6c0a5b231c96324ce52bdb9abc81e0850ae
KamilMammadov <kamiltm@code.edu.az>
2022-11-27 21:19:04 +0400

    final configurations

--- DemoEmailApp/Program.cs
+++ DemoEmailApp/Program.cs
@@ -48,68 +48,6 @@ namespace DemoEmailApp
 
 
 
-
-
-            //var builder = WebApplication.CreateBuilder(args);
-
-            //builder.Services.AddControllers();
-            //var emailConfig = builder.Configuration
-            // .GetSection("EmailConfiguration")
-            // .Get<EmailConfiguration>();
-            //builder.Services.AddSingleton(emailConfig);
-
-
-            //builder.Services
-
-            //   .AddDbContext<DataContext>(o =>
-            //   {
-            //       o.UseSqlServer("Server=DESKTOP-466PC0V;Database=DataEmail;Trusted_Connection=True;TrustServerCertificate=True;");
-            //   }, ServiceLifetime.Scoped)
-
-            //    .AddMvc();
-
-
-            //var app = builder.Build();
-
-            //app.UseStaticFiles();
-
-            //app.MapControllerRoute(
-            //    name: "default",
-            //    pattern: "{controller=Email}/{action=List}");
-
-            //app.Run();
-
-
-
-
-            //var builder = WebApplication.CreateBuilder(args);
-            //builder.Services
-
-            //         var emailConfig = builder.Configuration
-            //    .GetSection("EmailConfiguration")
-            //    .Get<EmailConfiguration>();
-            //builder.Services.AddSingleton(emailConfig);
-
-            //builder.Services.AddScoped<IEmailSender, EmailSender>();
-
-
-            //    .AddDbContext<DataContext>(o =>
-            //     {
-            //         o.UseSqlServer("Server=DESKTOP-466PC0V;Database=DataEmail;Trusted_Connection=True;TrustServerCertificate=True;");
-            //     }, ServiceLifetime.Transient)
-            //    .AddMvc();
-
-
-
-            //var app = builder.Build();
-
-            //app.UseStaticFiles();
-
-            //app.MapControllerRoute(
-            //    name: "default",
-            //    pattern: "{controller=home}/{action=index}");
-
-            //app.Run();
         }
     }
 }
